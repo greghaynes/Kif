5f09a57249b7c2d853385609b41274f0f4f03aa4
Gregory Haynes <greg@greghaynes.net>
2011-06-23 14:06:02 -0700

    Navbar has all buttons

--- kif.js
+++ kif.js
@@ -1,3 +1,60 @@
+function KifNavButton(name, canvas, start_x, start_y, type) {
+	this.start_x = start_x;
+	this.start_y = start_y;
+
+	this.jq = $("<div id=\"kif_navbar_"+name+"\" class=\"kif_navbutton\"></div>");
+	this.jq.appendTo("#kif_navbar");
+
+	this.canv_container = canvas.rect(start_x, start_y, 40, 40, 10);
+	this.canv_container.attr({fill: "#222", opacity: 0.5});
+	this.canv_doodle = canvas.set();
+
+	var canv_container = this.canv_container;
+	var canv_doodle = this.canv_doodle;
+
+	if(type == "next" || type == "last") {
+		canv_doodle.push(
+			canvas.path("M"+(start_x+10)+" "+(start_y+10)+"L"+(start_x+30)+" "+(start_y+20)+"L"+(start_x+10)+" "
+				+(start_y+30)+"L"+(start_x+10)+" "+(start_y+10)));
+
+		if(type == "last")
+			canv_doodle.push(canvas.path("M"+(start_x+32)+" "+(start_y+10)+"L"+(start_x+32)+" "+(start_y+32)));
+	} else if(type == "prev" || type == "first") {
+		canv_doodle.push(
+			canvas.path("M"+(start_x+10)+" "+(start_y+20)+"L"+(start_x+30)+" "+(start_y+30)+"L"+(start_x+30)+" "
+				+(start_y+10)+"L"+(start_x+10)+" "+(start_y+20)));
+
+		if(type == "first")
+			canv_doodle.push(canvas.path("M"+(start_x+8)+" "+(start_y+10)+"L"+(start_x+8)+" "+(start_y+32)));
+	}
+
+	canv_doodle.attr({stroke: "#DDD", fill: "#DDD", opacity: 0.5});
+
+	this.whole_button = canvas.set();
+	this.whole_button.push(canv_container, canv_doodle);
+
+	this.whole_button.hover(
+		function(event) {
+			canv_container.attr({opacity: 1.0});
+			canv_doodle.attr({opacity: 1.0});
+		},
+		function(event) {
+			canv_container.attr({opacity: 0.5});
+			canv_doodle.attr({opacity: 0.5});
+		});
+}
+
+function KifNavbar() {
+	this.jq = $("<div id=\"kif_navbar\"></div>");
+	this.jq.appendTo("#kif");
+	this.canvas = Raphael("kif_navbar", 200, 40);
+
+	this.first_button = new KifNavButton("first", this.canvas, 10, 0, "first");
+	this.prev_button = new KifNavButton("prev", this.canvas, 55, 0, "prev");
+	this.next_button = new KifNavButton("next", this.canvas, 100, 0, "next");
+	this.last_button = new KifNavButton("last", this.canvas, 145, 0, "last");
+}
+
 function Kif(pres_info) {
 	this.pres_info = pres_info;
 	
@@ -13,18 +70,8 @@ function Kif(pres_info) {
 }
 
 Kif.prototype.loadSlide = function(name) {
-	$("#kif").html("content");
 }
 
 Kif.prototype.loadNav = function() {
-	nav_elem = $("<div id=\"kif_nav\"></div>");
-	nav_elem.appendTo("#kif");
-
-	this.nav_canv = Raphael(10, 10, 200, 50);
-	var first_cont = this.nav_canv.rect(0, 0, 40, 40, 10);
-	first_cont.attr({fill: "#222", opacity: 0.5});
-	var first_arr = this.nav_canv.set();
-	first_arr.push(this.nav_canv.path("M10 10L30 20L10 30L10 10"),
-	               this.nav_canv.path("M32 10L32 32"));
-	first_arr.attr({stroke: "#DDD", fill: "#DDD", opacity: 0.5});
+	this.navbar = new KifNavbar();
 }
